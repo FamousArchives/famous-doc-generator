ffcae2147a6b853ce780ebfe944f519515c12fcf
tim <tim@famo.us>
2014-07-28 13:35:33 -0700

    packaged documentaiton generator

---
+++ bin/cmd.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+/*eslint no-process-exit:0*/
+'use strict';
+var argv = require('minimist')(process.argv.slice(2));
+var docGenerator = require('../lib');
+var filters = require('../lib/filters');
+var path = require('path');
+
+if (argv._.indexOf('help') > 0 || argv.h == true || argv.help == true) {
+  console.log('Famous Documentation Generator');
+  console.log('----------------');
+  console.log('Possible options');
+  console.log('----------------');
+  console.log('--base=[path] The directory to start searching from');
+  console.log('--out=[path] The directory where the compiled templates will write to.');
+  console.log('--outData=[path **OPTIONAL**] Path where the json data will be saved. Useful to debug templates');
+  process.exit(0);
+}
+
+var opts = {
+  baseDirectory: path.join(process.cwd(), argv.base),
+  templates: [
+    {
+      baseTemplate: path.join(__dirname, '../templates/doc.jade'),
+      outDirectory: path.join(process.cwd(), argv.out)
+    }
+  ],
+  filters: filters 
+}
+
+if (argv.outData) { 
+  opts.outData = argv.outData;
+}
+
+docGenerator(opts, function () {
+  console.log('documentation is generated.');
+})
