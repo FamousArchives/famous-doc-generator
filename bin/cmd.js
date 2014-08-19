3f486cfeeda228bce76a6c89be422a25451e43d9
Tim Chin <timjchin@gmail.com>
2014-08-19 15:08:27 -0700

    [feature] headerTemplate / footerTemplate
    —headerTemplate=./local/path/to/template
    —footerTemplate=./local/path/to/template
    
    This will render a jade template from the desired location as a header
    or footer, if provided.

--- bin/cmd.js
+++ bin/cmd.js
@@ -16,6 +16,8 @@ if (argv._.indexOf('help') > 0 || argv.h == true || argv.help == true || (argv._
   console.log('--ignore=[paths **OPTIONAL**] Add a directory nested underneath the base directory to ignore.');
   console.log('--outData=[path **OPTIONAL**] Path where the json data will be saved. Useful to debug templates');
   console.log('--pathPrefix=[path **OPTIONAL**] All asset pathing will get this prefix.');
+  console.log('--headerTemplate=[path **OPTIONAL**] Header partial to include.');
+  console.log('--footerTemplate=[path **OPTIONAL**] Footer partial to include.');
   process.exit(0);
 }
 
@@ -35,9 +37,18 @@ if (argv.outData) {
   opts.outData = argv.outData;
 }
 
-if (argv.pathPrefix) { 
+if (argv.pathPrefix) {
   opts.pathPrefix = argv.pathPrefix;
 }
+
+if (argv.headerTemplate) {
+  opts.headerTemplate = path.join(process.cwd(), argv.headerTemplate);
+}
+
+if (argv.footerTemplate) {
+  opts.footerTemplate = path.join(process.cwd(), argv.footerTemplate);
+}
+
 
 if (argv.ignore) {
   if (!(argv.ignore instanceof Array)) {
